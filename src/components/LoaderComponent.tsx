a723a249d65d78d589e7dd161c09abbedbf6aa50
Abel Borit <abelboritguitton@gmail.com>
2024-03-23 19:52:17 -0500

    [FEAT] create LoaderComponent structure and styles

--- src/components/LoaderComponent.tsx
+++ src/components/LoaderComponent.tsx
@@ -1,3 +1,19 @@
 export const LoaderComponent = () => {
-  return <div>LoaderComponent</div>;
+  return (
+    <div className="bg-primary-dark-900 w-screen h-screen flex flex-col gap-6 justify-center items-center">
+      <div className="loader">
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+        <div className="loader-square"></div>
+      </div>
+
+      <p className="text-2xl text-color-light tracking-[3px] text-loader">
+        -- Loading --
+      </p>
+    </div>
+  );
 };
